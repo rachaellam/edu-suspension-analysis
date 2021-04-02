c0d4c357567a85fd45d2d5b51c5cad64aecfba47
Rachael Lam <rachael.a.lam@gmail.com>
2021-04-02 17:02:25 -0400

    more analysis and variable changes

--- scripts/00-analysis-and-modelling.R
+++ scripts/00-analysis-and-modelling.R
@@ -16,6 +16,8 @@
 # install.packages("performance")
 # install.packages("see")
 # install.packages("broom")
+# install.packages("reshape2")
+# install.packages("knitr)
 
 library(here)
 library(devtools)
@@ -26,6 +28,8 @@ library(scales)
 library(performance)
 library(see)
 library(broom)
+library(reshape2)
+library(knitr)
 
 ### Load Dataset ###
 raw_data <- read.csv("inputs/data/CRDC-2015-16-School-Data.csv")
@@ -93,63 +97,96 @@ disc_high_sum <- disc_high %>%
   mutate(TOT_MULTOOS = select(., "TOT_DISCWODIS_MULTOOS_F", "TOT_DISCWODIS_MULTOOS_M")  %>% rowSums(na.rm = TRUE)) %>%
   mutate(TOT_SINGOOS = select(., "TOT_DISCWODIS_SINGOOS_F", "TOT_DISCWODIS_SINGOOS_M")  %>% rowSums(na.rm = TRUE)) %>%
   mutate(TOT_ENR = select(., "TOT_ENR_F", "TOT_ENR_M")  %>% rowSums(na.rm = TRUE)) %>%
-  transform(COST_PER_STUDENT_WOFED = round(SCH_SAL_TOTPERS_WOFED / TOT_ENR, digits = 2)) %>% # finding the cost per student
-  transform(COST_PER_STUDENT_TEACH_WOFED = round(SCH_SAL_TEACH_WOFED / TOT_ENR, digits = 2)) %>%
-  transform(COST_PER_STUDENT_AID_WOFED = round(SCH_SAL_AID_WOFED / TOT_ENR, digits = 2)) %>%
-  transform(COST_PER_STUDENT_SUP_WOFED = round(SCH_SAL_SUP_WOFED / TOT_ENR, digits = 2)) %>%
-  transform(COST_PER_STUDENT_ADM_WOFED = round(SCH_SAL_ADM_WOFED / TOT_ENR, digits = 2)) %>%
-  transform(COST_PER_STUDENT_NPE_WOFED = round(SCH_NPE_WOFED / TOT_ENR, digits = 2)) %>%
+  transform(SPEND_PER_STUDENT_WOFED = round(SCH_SAL_TOTPERS_WOFED / TOT_ENR, digits = 2)) %>% # finding the cost per student
+  transform(SPEND_PER_STUDENT_TEACH_WOFED = round(SCH_SAL_TEACH_WOFED / TOT_ENR, digits = 2)) %>%
+  transform(SPEND_PER_STUDENT_AID_WOFED = round(SCH_SAL_AID_WOFED / TOT_ENR, digits = 2)) %>%
+  transform(SPEND_PER_STUDENT_SUP_WOFED = round(SCH_SAL_SUP_WOFED / TOT_ENR, digits = 2)) %>%
+  transform(SPEND_PER_STUDENT_ADM_WOFED = round(SCH_SAL_ADM_WOFED / TOT_ENR, digits = 2)) %>%
+  transform(SPEND_PER_STUDENT_NPE_WOFED = round(SCH_NPE_WOFED / TOT_ENR, digits = 2)) %>%
   transform(TEACH_PER_STUDENT = round(TOT_ENR / SCH_FTE_TEACH_WOFED, digits = 2)) %>%
+  transform(DAYSMISSED_PER_100 = round(((TOT_DAYSMISSED / TOT_ENR) * 100), digits = 2)) %>%
   transform(ISS_PER_100 = round(((TOT_ISS / TOT_ENR) * 100), digits = 2)) %>%
   transform(MULTOOS_PER_100 = round(((TOT_MULTOOS / TOT_ENR) * 100), digits = 2)) %>%
   transform(SINGOOS_PER_100 = round(((TOT_SINGOOS / TOT_ENR) * 100), digits = 2)) %>%
-  select(LEA_STATE, LEA_STATE_NAME, LEAID, LEA_NAME, SCHID, SCH_NAME, SCH_NAME, COMBOKEY, TOT_ENR, TOT_DAYSMISSED, TOT_EXPWE, TOT_EXPWOE, TOT_ISS, ISS_PER_100, TOT_MULTOOS, MULTOOS_PER_100,
-         TOT_SINGOOS, SINGOOS_PER_100, SCH_SAL_TOTPERS_WOFED, SCH_SAL_TEACH_WOFED, SCH_SAL_AID_WOFED, SCH_SAL_SUP_WOFED, SCH_SAL_ADM_WOFED, COST_PER_STUDENT_WOFED, COST_PER_STUDENT_TEACH_WOFED,
-         COST_PER_STUDENT_AID_WOFED, COST_PER_STUDENT_SUP_WOFED, COST_PER_STUDENT_ADM_WOFED, COST_PER_STUDENT_NPE_WOFED, SCH_FTE_TEACH_WOFED, TEACH_PER_STUDENT)
+  select(LEA_STATE, LEA_STATE_NAME, LEAID, LEA_NAME, SCHID, SCH_NAME, SCH_NAME, COMBOKEY, TOT_ENR, TOT_DAYSMISSED, DAYSMISSED_PER_100, TOT_EXPWE, TOT_EXPWOE, TOT_ISS, 
+         ISS_PER_100, TOT_MULTOOS, MULTOOS_PER_100, TOT_SINGOOS, SINGOOS_PER_100, SCH_SAL_TOTPERS_WOFED, SCH_SAL_TEACH_WOFED, SCH_SAL_AID_WOFED, SCH_SAL_SUP_WOFED, 
+         SCH_SAL_ADM_WOFED, SPEND_PER_STUDENT_WOFED, SPEND_PER_STUDENT_TEACH_WOFED, SPEND_PER_STUDENT_AID_WOFED, SPEND_PER_STUDENT_SUP_WOFED, SPEND_PER_STUDENT_ADM_WOFED, 
+         SPEND_PER_STUDENT_NPE_WOFED, SCH_FTE_TEACH_WOFED, TEACH_PER_STUDENT) %>%
+  filter(SPEND_PER_STUDENT_SUP_WOFED < 1000000) # removing outlier of 2346486.911, leading me to believe that it was incorrect reporting
 
 # initial graphing
 disc_high_sum %>%
-  ggplot(aes(COST_PER_STUDENT_WOFED, TOT_DAYSMISSED)) +
+  ggplot(aes(SPEND_PER_STUDENT_WOFED, TOT_DAYSMISSED)) +
   geom_point() +
   scale_x_continuous(labels = comma) +
-  coord_cartesian(xlim = c(0, 25000)) # zoom into the 0-25000 dollar amount
+  coord_cartesian(xlim = c(0, 25000)) + # zoom into the 0-25000 dollar amount
+  theme_minimal()
 
 disc_high_sum %>%
-  ggplot(aes(COST_PER_STUDENT_TEACH_WOFED, TOT_DAYSMISSED)) +
+  ggplot(aes(SPEND_PER_STUDENT_TEACH_WOFED, TOT_DAYSMISSED)) +
   geom_point() +
   scale_x_continuous(labels = comma) +
-  coord_cartesian(xlim = c(0, 25000))
+  coord_cartesian(xlim = c(0, 25000)) +
+  theme_minimal()
 
 disc_high_sum %>%
-  ggplot(aes(COST_PER_STUDENT_AID_WOFED, TOT_DAYSMISSED)) +
+  ggplot(aes(SPEND_PER_STUDENT_AID_WOFED, TOT_DAYSMISSED)) +
   geom_point() +
   scale_x_continuous(labels = comma) +
-  coord_cartesian(xlim = c(0, 25000))
+  coord_cartesian(xlim = c(0, 25000)) +
+  theme_minimal()
 
 disc_high_sum %>%
-  ggplot(aes(COST_PER_STUDENT_WOFED, ISS_PER_100)) +
+  ggplot(aes(SPEND_PER_STUDENT_WOFED, ISS_PER_100)) +
   geom_point() +
-  scale_x_continuous(labels = comma)
+  scale_x_continuous(labels = comma) +
+  theme_minimal()
 
 disc_high_sum %>%
-  ggplot(aes(COST_PER_STUDENT_WOFED, MULTOOS_PER_100)) +
+  ggplot(aes(SPEND_PER_STUDENT_WOFED, MULTOOS_PER_100)) +
   geom_point() +
-  scale_x_continuous(labels = comma)
+  scale_x_continuous(labels = comma) +
+  theme_minimal()
 
 disc_high_sum %>%
-  ggplot(aes(COST_PER_STUDENT_WOFED, SINGOOS_PER_100)) +
+  ggplot(aes(SPEND_PER_STUDENT_WOFED, SINGOOS_PER_100)) +
   geom_point() +
-  scale_x_continuous(labels = comma)
+  scale_x_continuous(labels = comma) +
+  theme_minimal()
+
+disc_high_sum %>%
+  ggplot(aes(SPEND_PER_STUDENT_WOFED, DAYSMISSED_PER_100)) +
+  geom_point() +
+  scale_x_continuous(labels = comma) +
+  theme_minimal() +
+  coord_cartesian(xlim = c(0, 50000))
 
 # modelling
-lmper <- lm(ISS_PER_100 ~ COST_PER_STUDENT_WOFED + COST_PER_STUDENT_NPE_WOFED, data = disc_high_sum)
+lmper <- lm(ISS_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = disc_high_sum)
 summary(lmper)
 
-lmdays <- lm(TOT_DAYSMISSED ~ COST_PER_STUDENT_WOFED + COST_PER_STUDENT_NPE_WOFED, data = disc_high_sum)
+lmdays <- lm(DAYSMISSED_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = disc_high_sum)
 summary(lmdays)
 
-lmtype <- lm(ISS_PER_100 ~ COST_PER_STUDENT_TEACH_WOFED + COST_PER_STUDENT_AID_WOFED + COST_PER_STUDENT_SUP_WOFED + COST_PER_STUDENT_ADM_WOFED, data = disc_high_sum)
+lmtype <- lm(ISS_PER_100 ~ SPEND_PER_STUDENT_TEACH_WOFED + SPEND_PER_STUDENT_AID_WOFED + SPEND_PER_STUDENT_SUP_WOFED + SPEND_PER_STUDENT_ADM_WOFED, data = disc_high_sum)
 summary(lmtype)
+
+# overall numbers for salary spend
+salary <- disc_high_sum %>%
+  select(SPEND_PER_STUDENT_TEACH_WOFED, SPEND_PER_STUDENT_AID_WOFED, SPEND_PER_STUDENT_SUP_WOFED, SPEND_PER_STUDENT_ADM_WOFED) %>%
+  melt() %>%
+  group_by(variable) %>%
+  summarize(the_min = min(value),
+            the_max = max(value),
+            the_mean = mean(value),
+            the_median = median(value),
+            std_dev = sd(value))
+
+salary %>%
+  kable(digits = 2,
+        caption = "Salary Overview Across All Schools",
+        col.names = c ("Spend Per Student", "Min", "Max", "Mean", "Median", "Standard Deviation"),
+        align = c('l', 'l', 'l', 'l', 'l'))
 
 # add a few single states to see if it supports your overall population results
 # 10 most populous states
@@ -157,98 +194,131 @@ summary(lmtype)
 ny <- disc_high_sum %>%
   filter(LEA_STATE_NAME == "NEW YORK")
 
-lmny <- lm(TOT_DAYSMISSED ~ COST_PER_STUDENT_WOFED, data = ny)
+lmny <- lm(ISS_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = ny)
 summary(lmny)
 
-lmnyiss <- lm(ISS_PER_100~ COST_PER_STUDENT_WOFED, data = ny)
-summary(lmnyiss)
+lmnydays <- lm(DAYSMISSED_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = ny)
+summary(lmnydays)
 
 # pennsylvania (reg)
 pa <- disc_high_sum %>%
   filter(LEA_STATE_NAME == "PENNSYLVANIA")
 
-lmpa <- lm(TOT_DAYSMISSED ~ COST_PER_STUDENT_WOFED, data = pa)
+lmpa <- lm(ISS_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = pa)
 summary(lmpa)
 
-lmpaiss <- lm(ISS_PER_100 ~ COST_PER_STUDENT_WOFED, data = pa)
-summary(lmpaiss)
+lmpadays <- lm(DAYSMISSED_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = pa)
+summary(lmpadays)
 
 # illinois (reg)
 il <- disc_high_sum %>%
   filter(LEA_STATE_NAME == "ILLINOIS")
 
-lmil <- lm(TOT_DAYSMISSED ~ COST_PER_STUDENT_WOFED, data = il)
+lmil <- lm(ISS_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = il)
 summary(lmil)
 
-lmiliss <- lm(ISS_PER_100 ~ COST_PER_STUDENT_WOFED, data = il)
-summary(lmiliss)
+lmildays <- lm(DAYSMISSED_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = il)
+summary(lmildays)
 
 # ohio (pro)
 oh <- disc_high_sum %>%
   filter(LEA_STATE_NAME == "OHIO")
 
-lmoh <- lm(TOT_DAYSMISSED ~ COST_PER_STUDENT_WOFED, data = oh)
+lmoh <- lm(ISS_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = oh)
 summary(lmoh)
 
-lmohiss <- lm(ISS_PER_100 ~ COST_PER_STUDENT_WOFED, data = oh)
-summary(lmohiss)
+lmohdays <- lm(DAYSMISSED_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = oh)
+summary(lmohdays)
 
 # texas (reg)
 tx <- disc_high_sum %>%
   filter(LEA_STATE_NAME == "TEXAS")
 
-lmtx <- lm(TOT_DAYSMISSED ~ COST_PER_STUDENT_WOFED, data = tx)
+lmtx <- lm(ISS_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = tx)
 summary(lmtx)
 
-lmtxiss <- lm(ISS_PER_100 ~ COST_PER_STUDENT_WOFED, data = tx)
-summary(lmtxiss)
+lmtxdays <- lm(DAYSMISSED_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = tx)
+summary(lmtxdays)
 
 # florida (reg)
 fl <- disc_high_sum %>%
   filter(LEA_STATE_NAME == "FLORIDA")
 
-lmfl <- lm(TOT_DAYSMISSED ~ COST_PER_STUDENT_WOFED, data = fl)
+lmfl <- lm(ISS_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = fl)
 summary(lmfl)
 
-lmfliss <- lm(ISS_PER_100 ~ COST_PER_STUDENT_WOFED, data = fl)
-summary(lmfliss)
+lmfldays <- lm(DAYSMISSED_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = fl)
+summary(lmfldays)
 
 # california (pro)
 ca <- disc_high_sum %>%
   filter(LEA_STATE_NAME == "CALIFORNIA")
 
-lmca <- lm(TOT_DAYSMISSED ~ COST_PER_STUDENT_WOFED, data = ca)
+lmca <- lm(ISS_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = ca)
 summary(lmca)
 
-lmcaiss <- lm(ISS_PER_100 ~ COST_PER_STUDENT_WOFED, data = ca)
-summary(lmcaiss)
+lmcadays <- lm(DAYSMISSED_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = ca)
+summary(lmcadays)
 
 # georgia
 ga <- disc_high_sum %>%
   filter(LEA_STATE_NAME == "GEORGIA")
 
-lmga <- lm(TOT_DAYSMISSED ~ COST_PER_STUDENT_WOFED, data = ga)
+lmga <- lm(ISS_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = ga)
 summary(lmga)
 
-lmgaiss <- lm(ISS_PER_100 ~ COST_PER_STUDENT_WOFED, data = ga)
-summary(lmgaiss)
+lmgadays <- lm(DAYSMISSED_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = ga)
+summary(lmgadays)
 
 # north carolina
 nc <- disc_high_sum %>%
   filter(LEA_STATE_NAME == "NORTH CAROLINA")
 
-lmnc <- lm(TOT_DAYSMISSED ~ COST_PER_STUDENT_WOFED, data = nc)
+lmnc <- lm(ISS_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = nc)
 summary(lmnc)
 
-lmnciss <- lm(ISS_PER_100 ~ COST_PER_STUDENT_WOFED, data = nc)
-summary(lmnciss)
+lmncdays <- lm(DAYSMISSED_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = nc)
+summary(lmncdays)
 
 # michigan
 mi <- disc_high_sum %>%
   filter(LEA_STATE_NAME == "MICHIGAN")
 
-lmmi <- lm(TOT_DAYSMISSED ~ COST_PER_STUDENT_WOFED, data = mi)
+lmmi <- lm(ISS_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = mi)
 summary(lmmi)
 
-lmmiiss <- lm(ISS_PER_100 ~ COST_PER_STUDENT_WOFED, data = mi)
-summary(lmmiiss)
+lmmidays <- lm(DAYSMISSED_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = mi)
+summary(lmmidays)
+
+rep <- disc_high_sum %>%
+  filter(LEA_STATE_NAME == "ALABAMA" | LEA_STATE_NAME == "ALASKA" | LEA_STATE_NAME == "ARIZONA" | LEA_STATE_NAME == "ARKANSAS" | LEA_STATE_NAME == "FLORIDA"
+         | LEA_STATE_NAME == "GEORGIA" | LEA_STATE_NAME == "IDAHO" | LEA_STATE_NAME == "INDIANA" | LEA_STATE_NAME == "IOWA" | LEA_STATE_NAME == "KANSAS"
+         | LEA_STATE_NAME == "KENTUCKY" | LEA_STATE_NAME == "LOUISIANA" | LEA_STATE_NAME == "MICHIGAN" | LEA_STATE_NAME == "MISSISSIPPI"
+         | LEA_STATE_NAME == "MISSOURI" | LEA_STATE_NAME == "MONTANA" | LEA_STATE_NAME == "NEBRASKA" | LEA_STATE_NAME == "NORTH CAROLINA"
+         | LEA_STATE_NAME == "NORTH DAKOTA" | LEA_STATE_NAME == "OHIO" | LEA_STATE_NAME == "OKLAHOMA" | LEA_STATE_NAME == "PENNSYLVANIA"
+         | LEA_STATE_NAME == "SOUTH CAROLINA" | LEA_STATE_NAME == "SOUTH DAKOTA" | LEA_STATE_NAME == "TENNESSEE" | LEA_STATE_NAME == "TEXAS"
+         | LEA_STATE_NAME == "UTAH" | LEA_STATE_NAME == "WEST VIRGINIA" | LEA_STATE_NAME == "WISCONSIN" | LEA_STATE_NAME == "WYOMING")
+
+dem <- disc_high_sum %>%
+  filter(LEA_STATE_NAME == "CALIFORNIA" | LEA_STATE_NAME == "COLORADO" | LEA_STATE_NAME == "CONNECTICUT" | LEA_STATE_NAME == "DELAWARE"
+         | LEA_STATE_NAME == "DISTRICT OF COLUMBIA" | LEA_STATE_NAME == "HAWAII" | LEA_STATE_NAME == "ILLINOIS" | LEA_STATE_NAME == "IOWA"
+         | LEA_STATE_NAME == "MAINE" | LEA_STATE_NAME == "MARYLAND" | LEA_STATE_NAME == "MASSACHUSETTS" | LEA_STATE_NAME == "MINNESOTA"
+         | LEA_STATE_NAME == "NEVADA" | LEA_STATE_NAME == "NEW HAMPSHIRE" | LEA_STATE_NAME == "NEW JERSEY" | LEA_STATE_NAME == "NEW MEXICO"
+         | LEA_STATE_NAME == "NEW YORK" | LEA_STATE_NAME == "OREGON" | LEA_STATE_NAME == "RHODE ISLAND" | LEA_STATE_NAME == "VERMONT"
+         | LEA_STATE_NAME == "VIRGINIA" | LEA_STATE_NAME == "WASHINGTON")
+
+lmrep <- lm(ISS_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = rep)
+summary(lmrep)
+
+lmrepdays <- lmdays <- lm(DAYSMISSED_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = rep)
+summary(lmrepdays)
+
+lmdem <- lm(ISS_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = dem)
+summary(lmdem)
+
+lmdemdays <- lm(DAYSMISSED_PER_100 ~ SPEND_PER_STUDENT_WOFED + SPEND_PER_STUDENT_NPE_WOFED, data = dem)
+summary(lmdemdays)
+
+
+
+
